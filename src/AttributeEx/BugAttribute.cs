8880e27da1a1b8ebc3e805242b26077bb63632da
Pratap Lakshman <pratapl@microsoft.com>
2019-02-07 11:07:55 +0530

    Added BugAttribute.

---
+++ src/AttributeEx/BugAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.TestFramework.AttributeEx
+{
+    // -------------------------------------------------------------------------------------------
+    // A strongly typped "Note" attribute that allows attaching a string as a note to a testmethod
+    // E.g.
+    // [TestMethod]
+    // [Note("CPU Intensive")]
+    // public void myTestCase() { }
+    // -------------------------------------------------------------------------------------------
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class BugAttribute : TestPropertyAttribute
+    {
+        public BugAttribute(string s)
+            : base("Bug", s)
+        {
+        }
+    }
+}
