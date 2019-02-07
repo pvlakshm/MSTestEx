57645ab6d017bbbed8ca8973a66a781e1fde277b
Pratap Lakshman <pratapl@microsoft.com>
2019-02-07 10:45:09 +0530

    Adding NoteAttrobute.

---
+++ src/AttributeEx/NoteAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.TestFramework.AttributeEx
+{
+    // -------------------------------------------------------------------------------------------
+    // A strongly typped "Note" attribute that allows attaching a string as a note to a testmethod
+    // E.g.
+    // [TestMethod]
+    // [Note("CPU Intensive")]
+    // public void myTestCase() { }
+    // -------------------------------------------------------------------------------------------
+
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = true)]
+    public class NoteAttribute : TestPropertyAttribute
+    {
+        public NoteAttribute(string s)
+            : base("Note", s)
+        {
+        }
+    }
+}
