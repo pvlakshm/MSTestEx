862e2bda71499220ba10e2f4b03894f47cbb3eac
Pratap Lakshman <pratapl@microsoft.com>
2019-02-12 16:21:36 +0530

    Updated folder structure.  Added attributes for Author, BugID, and Description.  Added RetriableTestMethod.

---
+++ src/MSTest.TestFramework.Extensions/AttribEx/DescriptionAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MSTest.TestFramework.Extensions.AttributeEx
+{
+    /// <summary>Add descriptive text to a test. Supported a TestMethod and a TestClass.</summary>
+    /// <param name="desc">description as a string</param>"
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
+    public class DescriptionAttribute : TestPropertyAttribute
+    {
+        public DescriptionAttribute(string s)
+            : base("Description", s)
+        {
+        }
+    }
+}
