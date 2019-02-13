6ac5239cc4898a0277cb167474afd808c24acf37
Pratap Lakshman <pvlakshm@hotmail.com>
2019-02-14 00:38:50 +0530

    BugID is now an int, and multiple Bug attributes may be added to a testmethod.

--- src/MSTest.TestFramework.Extensions/AttribEx/AuthorAttribute.cs
+++ src/MSTest.TestFramework.Extensions/AttribEx/AuthorAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace MSTest.TestFramework.Extensions.AttributeEx
 {
-    /// <summary>Add information about the author of the tests. Supported a TestMethod and a TestClass.</summary>
+    /// <summary>Add an author to a test method or a test class.</summary>
     /// <param name="name">author name as a string</param>"
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false)]
     public class AuthorAttribute : TestPropertyAttribute
