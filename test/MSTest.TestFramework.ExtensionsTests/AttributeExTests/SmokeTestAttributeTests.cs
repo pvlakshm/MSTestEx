b4943e49ff2a47eee2c829aab7b4f9c8365b6018
Pratap Lakshman <pvlakshm@hotmail.com>
2019-02-21 20:02:16 +0530

    Adding tests for Author, Bug, and SmokeTest attributes.

---
+++ test/MSTest.TestFramework.ExtensionsTests/AttributeExTests/SmokeTestAttributeTests.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MSTest.TestFramework.Extensions.AttributeEx;
+using MSTest.TestFramework.Extensions.TestMethodEx;
+using Moq;
+using System.Linq;
+using System;
+
+namespace MSTest.TestFramework.ExtensionsTests.AttributeExTests
+{
+    [TestClass]
+    public class SmokeTestAttributeTests
+    {
+        [TestMethod]
+        public void TestCanHaveSingleSmokeTestAttributeAndIsExecutedOnlyOnce()
+        {
+            // Arrange
+            var mockPassingTestMethod = new Mock<ITestMethod>();
+
+            var args = It.IsAny<object[]>();
+            var passingTestResult = new TestResult() { Outcome = UnitTestOutcome.Passed };
+            mockPassingTestMethod.Setup(tm => tm.Invoke(args)).Returns(passingTestResult);
+
+            mockPassingTestMethod.Setup(tm => tm.GetAllAttributes(false)).Returns(() =>
+                {
+                    Attribute[] attr = {
+                        new SmokeTestAttribute()
+                    };
+                    return attr;
+                });
+
+            var tmx = new TestMethodExAttribute();
+
+            // Act
+            var tr = tmx.Execute(mockPassingTestMethod.Object);
+
+            // Assert
+            mockPassingTestMethod.Verify(tm => tm.Invoke(args), Times.Once);
+            Assert.AreEqual(1, tr.Length);
+            Assert.AreEqual(UnitTestOutcome.Passed, tr.First().Outcome);
+        }
+    }
+}
