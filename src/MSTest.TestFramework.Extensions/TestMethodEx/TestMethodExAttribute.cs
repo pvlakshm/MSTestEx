68102b9c79cd2c37c7cabb6256177e65be3bb0fe
Pratap Lakshman <pvlakshm@hotmail.com>
2019-02-22 02:20:31 +0530

    Retry on a test method throwing an expetion will happen only if it is an ExpectedException.

--- src/MSTest.TestFramework.Extensions/TestMethodEx/TestMethodExAttribute.cs
+++ src/MSTest.TestFramework.Extensions/TestMethodEx/TestMethodExAttribute.cs
@@ -9,56 +9,115 @@ namespace MSTest.TestFramework.Extensions.TestMethodEx
     [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
     public class TestMethodExAttribute : TestMethodAttribute
     {
+        //public override TestResult[] Execute(ITestMethod testMethod)
+        //{
+        //    // NOTE
+        //    // This implementation will need to be refactored as we add more
+        //    // execution variations.
+
+        //    TestResult[] result = null;
+        //    Attribute[] attr = testMethod.GetAllAttributes(false);
+
+        //    if (attr == null)
+        //    {
+        //        result = base.Execute(testMethod);
+        //        return result;
+        //    }
+
+        //    foreach (Attribute a in attr)
+        //    {
+        //        if (a is RetryAttribute)
+        //        {
+        //            RetryAttribute retryAttr = (RetryAttribute)a;
+        //            int retryCount = int.Parse(retryAttr.Value);
+
+        //            var currentCount = 0;
+
+        //            while (currentCount < retryCount)
+        //            {
+        //                try
+        //                {
+        //                    result = base.Execute(testMethod);
+        //                }
+        //                catch (Exception) { }
+
+        //                currentCount++;
+
+        //                if ((result == null) ||
+        //                       (result.Any((tr) => tr.Outcome == UnitTestOutcome.Failed)))
+        //                {
+        //                    continue;
+        //                }
+        //                else
+        //                {
+        //                    break;
+        //                }
+        //            }
+
+        //            return result;
+        //        }
+        //    }
+
+        //    result = base.Execute(testMethod);
+        //    return result;
+        //}
+
         public override TestResult[] Execute(ITestMethod testMethod)
         {
             // NOTE
             // This implementation will need to be refactored as we add more
             // execution variations.
 
-            TestResult[] result = null;
+            int retryCount = 1;
+            Type eet = typeof(Exception);
             Attribute[] attr = testMethod.GetAllAttributes(false);
 
-            if (attr == null)
+            if (attr != null)
             {
-                result = base.Execute(testMethod);
-                return result;
-            }
-
-            foreach (Attribute a in attr)
-            {
-                if (a is RetryAttribute)
+                foreach (Attribute a in attr)
                 {
-                    RetryAttribute retryAttr = (RetryAttribute)a;
-                    int retryCount = int.Parse(retryAttr.Value);
-
-                    var currentCount = 0;
-
-                    while (currentCount < retryCount)
+                    if (a is RetryAttribute)
                     {
-                        try
-                        {
-                            result = base.Execute(testMethod);
-                        }
-                        catch (Exception) { }
-
-                        currentCount++;
-
-                        if ((result == null) ||
-                               (result.Any((tr) => tr.Outcome == UnitTestOutcome.Failed)))
-                        {
-                            continue;
-                        }
-                        else
-                        {
-                            break;
-                        }
+                        RetryAttribute retryAttr = (RetryAttribute)a;
+                        retryCount = int.Parse(retryAttr.Value);
                     }
 
-                    return result;
+                    if (a is ExpectedExceptionAttribute)
+                    {
+                        ExpectedExceptionAttribute eea = (ExpectedExceptionAttribute) a;
+                        eet = eea.ExceptionType;
+                    }
                 }
             }
 
-            result = base.Execute(testMethod);
+            TestResult[] result = null;
+
+            var currentCount = 0;
+            while (currentCount < retryCount)
+            {
+                currentCount++;
+
+                try
+                {
+                    result = base.Execute(testMethod);
+                }
+                catch (Exception e)
+                {
+                    if (e.GetType().Equals(eet) == false)
+                    {
+                        throw;
+                    }
+                }
+
+                if ((result == null) ||
+                       (result.Any((tr) => tr.Outcome == UnitTestOutcome.Failed)))
+                {
+                    continue;
+                }
+
+                break;
+            }
+
             return result;
         }
     }
