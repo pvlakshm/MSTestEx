c2b43a8571c3a555c301225ab2dd15c7a1b9aad0
Pratap Lakshman <pvlakshm@hotmail.com>
2019-03-10 23:38:41 +0530

    Adding tests for Repeat + Retry composition.

--- test/MSTest.TestFramework.ExtensionsTests/AttributeExTests/RepeatAttributeTests.cs
+++ test/MSTest.TestFramework.ExtensionsTests/AttributeExTests/RepeatAttributeTests.cs
@@ -77,5 +77,75 @@ namespace MSTest.TestFramework.ExtensionsTests.AttributeExTests
             Assert.AreEqual(tr.Length, expectedExecutionAttempts);
             Assert.IsTrue((tr.All((r) => r.Outcome == requiredTestOutCome)));
         }
+
+        [TestMethod()]
+        [DataRow(UnitTestOutcome.Failed, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Failed, 5, 5, 5)]
+        [DataRow(UnitTestOutcome.Failed, 50, 5, 5)]
+        [DataRow(UnitTestOutcome.Inconclusive, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Inconclusive, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.Inconclusive, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.Passed, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Passed, 5, 5, 5)]
+        [DataRow(UnitTestOutcome.Passed, 50, 5, 50)]
+        [DataRow(UnitTestOutcome.InProgress, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.InProgress, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.InProgress, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.Error, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Error, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.Error, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.Timeout, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Timeout, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.Timeout, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.Aborted, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Aborted, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.Aborted, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.Unknown, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.Unknown, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.Unknown, 50, 5, 1)]
+        [DataRow(UnitTestOutcome.NotRunnable, 1, 1, 1)]
+        [DataRow(UnitTestOutcome.NotRunnable, 5, 5, 1)]
+        [DataRow(UnitTestOutcome.NotRunnable, 50, 5, 1)]
+        public void RepeatWithRetryTestForAllTestOutcomes(
+            UnitTestOutcome requiredTestOutCome,
+            int requestedRepeatCount,
+            int requestedRetryCount,
+            int expectedExecutionAttempts)
+        {
+            // Arrange
+            TestResult[] expected =
+                {
+                    new TestResult() { Outcome = requiredTestOutCome }
+                };
+
+            var mockTestMethod = new Mock<ITestMethod>();
+            mockTestMethod.Setup(tm => tm.GetAllAttributes(false)).Returns(() =>
+            {
+                Attribute[] attr =
+                    {
+                        new RepeatAttribute(requestedRepeatCount),
+                        new RetryAttribute(requestedRetryCount),
+                    };
+                    return attr;
+                }
+            );
+
+            var args = It.IsAny<object[]>();
+            mockTestMethod.Setup(tm => tm.Invoke(args)).Returns(() =>
+                {
+                    return expected[0];
+                }
+            );
+
+
+            // Act
+            var retriableTestMethod = new TestMethodExAttribute();
+            var tr = retriableTestMethod.Execute(mockTestMethod.Object);
+
+            // Assert
+            mockTestMethod.Verify(tm => tm.Invoke(args), Times.Exactly(expectedExecutionAttempts));
+            Assert.AreEqual(tr.Length, expectedExecutionAttempts);
+            Assert.IsTrue((tr.All((r) => r.Outcome == requiredTestOutCome)));
+        }
     }
 }
