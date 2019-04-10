6b24a536ec85e43c48ecfe98aa838d7d06606a66
Pratap Lakshman <pvlakshm@hotmail.com>
2019-04-10 12:17:50 +0530

    Added IntRandomAttribute test

--- test/MSTest.TestFramework.ExtensionsTests/AttributeExTests/RandomAttributeTests.cs
+++ test/MSTest.TestFramework.ExtensionsTests/AttributeExTests/RandomAttributeTests.cs
@@ -4,14 +4,14 @@ using MSTest.TestFramework.Extensions.AttributeEx;
 namespace MSTest.TestFramework.ExtensionsTests.AttributeExTests
 {
     [TestClass]
-    public class RandomAttributeTests
+    public class IntRandomAttributeTests
     {
         [TestMethod]
         [RandomAttribute(1, 2, 10)]
         public void add(int x, int y)
         {
-            Assert.IsTrue(1 <= x && x <= 2);
-            Assert.IsTrue(1 <= y && y <= 2);
+            Assert.IsTrue(1 <= x && x < 2);
+            Assert.IsTrue(1 <= y && y < 2);
         }
     }
 }
